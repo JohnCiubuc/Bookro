1b3f5dc2211b0154206ee3426c62c4b1ce00afe5
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-05-04 23:11:25 -0500

    Simplified triggerKeyStatus

--- src/Bookro.cpp
+++ src/Bookro.cpp
@@ -8,20 +8,11 @@ Bookro::Bookro(QWidget *parent)
 {
     ui->setupUi(this);
     this->hide();
-    debugTimer = new QTimer;
     Listener = new EvDevKeyboardListener();
     ScriptEngine = new ScriptEngineLite();
 
-
     connect(Listener, &EvDevKeyboardListener::evdevKey, this, &Bookro::keyboardListener);
-//    connect(debugTimer, &QTimer::timeout, this, [=]()
-//    {
-//        input->sendKey(0x33, UInput::Keypress);
-//        db "test";
-//        input->sendKey(0x33, UInput::Release);
-////        as->sendKeyPressEvent(0x4A);
-//    });
-//    debugTimer->start(1000);
+
 }
 
 Bookro::~Bookro()
@@ -33,12 +24,8 @@ void Bookro::keyboardListener(int key, int keyState)
 {
     QString keyName = ScriptEngine->getDikName(key);
     db keyName <<   ", " << keyState;
-    if (keyName == "DIK_APOSTROPHE" && keyState == 1)
-    {
-        _triggerKeyStatus=1;
-    }
-    if (keyName == "DIK_APOSTROPHE" && keyState == 0)
-        _triggerKeyStatus=0;
+    if (keyName == "DIK_APOSTROPHE")
+        _triggerKeyStatus = keyState;
 
     if (keyName != "DIK_APOSTROPHE" && _triggerKeyStatus == 1 && keyState == 1)
     {
